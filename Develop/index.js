4d13f5c1e2db5aa3f00a528093afb829a5b376b4
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-28 13:07:13 -0400

    installed npm & packages / required node fs module & inquirer npm package

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,30 @@
+console.log('js connected / node.js working')
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the Title of your Project?',
+                name: 'projectTitle'
+            }
+        ]).then((response) => {
+            console.log(typeof(response.projectTitle));
+        })
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', '# Hello', (err) => {
+        console.log('Writing file...');
+        console.log('File Written!');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
