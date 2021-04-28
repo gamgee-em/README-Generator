0d34e9b98710c72808ae991fed0b4af21bbfae7f
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-28 18:55:27 -0400

    now writing README.md file and populating some user input

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+
+# **Readme.md**
+
+## **Description**
+* CL application that assists the User in building a professional README.md file for their project.
+
+## Table of Contents
+
+* Installation(#installation)
+* Usage(#usage)
+* Credits(#credits)
+* License(#license)
+
+## Installation
+* npm init -y / npm i inquirer
+
+## Usage
+* undefined
+
+## ScreenShot
+undefined
+
+## Licence
+
+## Contributing
+
+## Tests
+
+## Questions
+
+If you would like to contribute to the project please contact me @ 
+* [GitHub](https://github.com/gamgee-em)
+* [Email](mailto:samsweigart@hotmail.com)
--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 console.log('js connected / node.js working')
+const { Console } = require('console');
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -6,28 +7,98 @@ const { title } = require('process');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is the Title of your Project?',
-                name: 'projectTitle'
-            }
-        ]).then((response) => {
-            console.log(typeof(response.projectTitle));
-        })
+    {
+        type: 'input',
+        message: 'What is the Title of your Project? ',
+        name: 'projectTitle'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project: ',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please list the installation commands:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Enter a valid e-mail where potential contributors can contact you with questions regarding your repo:',
+        name: 'email'
+    }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('README.md', '# Hello', (err) => {
-        console.log('Writing file...');
-        console.log('File Written!');
-    })
+const getAnswers = (answers) => {
+    inquirer.prompt(questions, answers)
+        .then((response) => {
+            console.log(response);
+            //writeToFile(response);
+            renderMarkUp(response);
+    });
+};
+
+
+
+
+const renderMarkUp = (response) => {
+const markUp = `
+# **${response.projectTitle}**
+
+## **Description**
+* ${response.description}
+
+## Table of Contents
+
+* Installation(#installation)
+* Usage(#usage)
+* Credits(#credits)
+* License(#license)
+
+## Installation
+* ${response.installation}
+
+## Usage
+* ${response.usage}
+
+## ScreenShot
+${response.screenshot}
+
+## Licence
+
+## Contributing
+
+## Tests
+
+## Questions
+
+If you would like to contribute to the project please contact me @ 
+* [GitHub](https://github.com/${response.github})
+* [Email](mailto:${response.email})
+` 
+writeToFile(markUp);
+};
+
+const writeToFile = (markUp) => {
+    fs.writeFile('README.md', markUp.toString(), (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log(`File Written!`);
+        }
+    });
 }
 
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
+// TODO: Create a function to initialize app
+function init() {
+    getAnswers();
+}
+
 init();
